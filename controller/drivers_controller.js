7e62c37a68eab2262e0be136fcab45f4c604498c
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-05-01 17:04:13 +0700

    The BodyParser Middleware

--- controller/drivers_controller.js
+++ controller/drivers_controller.js
@@ -1,5 +1,8 @@
 module.exports = {
   greeting(req, res) {
     res.send({ hi: 'there' })
+  },
+  create(req, res) {
+
   }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,6 @@ module.exports = (app) => {
   // Watch for incoming request method GET
   // to the route http://localhost:3050/api
   app.get('/api', DriverController.greeting)
+
+  app.post('/api/drivers', DriverController.create)
 }
