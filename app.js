64fa188d9b3ee965a4dabba22e926069026349b8
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-05-01 14:46:10 +0700

    Handling Requests with Express and test

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const app = express()
 
+// Watch for incoming request method GET
+// to the route http://localhost:3050/api
+app.get('/api', (req, res) => {
+  res.send({ hi: 'there' })
+})
+
 module.exports = app
