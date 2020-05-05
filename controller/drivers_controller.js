d3df1e16d71d280f444b442cb4c8809c60424578
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-05-05 15:41:53 +0700

    Middlewares and Handling Editing of Drivers

--- controller/drivers_controller.js
+++ controller/drivers_controller.js
@@ -4,9 +4,19 @@ module.exports = {
   greeting(req, res) {
     res.send({ hi: 'there' })
   },
-  create(req, res) {
+  create(req, res, next) {
     const driverProps = req.body
     Driver.create(driverProps)
       .then(driver => res.send(driver))
+      .catch(next)
+  },
+  edit(req, res, next) {
+    const driverId = req.params.driverId
+    const driverProps = req.body
+
+    Driver.findByIdAndUpdate({ _id: driverId }, driverProps)
+      .then((driver) => Driver.findById({ _id: driverId }))
+      .then(driver => res.send(driver))
+      .catch(next)
   }
 }
